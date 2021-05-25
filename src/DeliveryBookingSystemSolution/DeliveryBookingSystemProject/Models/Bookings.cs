5ae530762cf5913faab8ed00b1aa882455d691d9
Prasanna2105 <poonkuzali.prasanna21@gmail.com>
2021-05-25 19:01:39 +0530

    Project

---
+++ src/DeliveryBookingSystemSolution/DeliveryBookingSystemProject/Models/Bookings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryBookingSystemProject.Models
+{
+    public class Bookings
+    {
+        [Key]
+        public int OrderID { get; set; }
+        
+        public int CustomerId { get; set; }
+        
+        public int ExecutiveId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime TimeOfPickup { get; set; }
+        public string Status { get; set; }
+        public int WeightOfPackage { get; set; }
+        public float Price { get; set; }
+    }
+}
